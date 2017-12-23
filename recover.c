bc8f3655cd877233d5d221cce4a30179c3da42bd
deepashree296 <deepashree296@users.noreply.github.com>
2017-12-23 07:16:52 +0000

    20171223T071645Z

--- recover.c
+++ recover.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
 {
 
     typedef uint8_t  BYTE;
+
     // ensure proper usage
     if (argc != 2)
     {
@@ -14,10 +15,9 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // open memory card file
-
-    // remember filenames
+    // remember filename
     char *infile = argv[1];
+
     // open input file
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
@@ -28,7 +28,6 @@ int main(int argc, char *argv[])
 
 
     BYTE buffer[512];
-
     char filename[8];
     int jpeg_count = -1;
     bool start_of_a_new_jpeg = false;
@@ -36,20 +35,18 @@ int main(int argc, char *argv[])
 
     while (fread(&buffer, sizeof(buffer), 1, inptr) > 0)
     {
-        // read one chunk of 512 bytes in a buffer
-        //fread(&buffer, 512, 1, inptr);
+        // setting start of jpeg as false each time new bytes are read
         start_of_a_new_jpeg = false;
-        //printf("Reading %d", (int)fread(buffer, 512 , 1, inptr));
 
         // find beginning of JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            //printf("Reading new buffer %d %d %d %d\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+            // if found new jpeg, setting flag to true
             start_of_a_new_jpeg = true;
             jpeg_count++;
         }
 
-        // will not start till we found our first JPEG
+        // whenever new jpeg found, will close the file and open a new file
         if (start_of_a_new_jpeg)
         {
             if ( img != NULL )
@@ -60,15 +57,13 @@ int main(int argc, char *argv[])
             sprintf(filename, "%03i.jpg", jpeg_count);
             img = fopen(filename, "w");
 
-
         }
 
+        // will keep appending bytes to the already opened file
         if ( img != NULL )
         {
             fwrite(&buffer, 512, 1, img);
         }
-
-
     }
     if (img != NULL)
     {
@@ -79,5 +74,4 @@ int main(int argc, char *argv[])
         fclose(inptr);
     }
     return 0;
-
 }
